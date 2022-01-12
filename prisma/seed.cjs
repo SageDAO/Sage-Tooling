b53d8fe7b078a4c6f9a105f223f9de40ef5c408e
Dante <23104993+fornax2@users.noreply.github.com>
2022-01-11 23:18:02 -0300

    limit rewards seed data

--- prisma/seed.cjs
+++ prisma/seed.cjs
@@ -12,6 +12,7 @@ async function main() {
       contract: '0x74b988156925937bd4e082f0ed7429da8eaea8db',
       startingBlock: 13649693,
       lastBlockInspected: 13942339,
+      positionSizeLimit: "100000000000000000000000"
     }
   });
 
@@ -25,6 +26,7 @@ async function main() {
       contract: '0xd1790c5435b9fb7c9444c749cefe53d40d751eac',
       startingBlock: 22976955,
       lastBlockInspected: 27015508,
+      positionSizeLimit: "100000000000000000000000"
     }
   });
 
@@ -38,6 +40,7 @@ async function main() {
       contract: '0x230319cbd36fd911c5537254c99613b447b8a178',
       startingBlock: 25732987,
       lastBlockInspected: 27015508,
+      positionSizeLimit: "100000000000000000000000"
     }
   });
   
