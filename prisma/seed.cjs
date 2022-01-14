ba8bedbcc44aa5e10ecd223de2744ce9bf47cbf5
Dante <23104993+fornax2@users.noreply.github.com>
2022-01-13 22:32:45 -0300

    fix initial lastBlockInspected

--- prisma/seed.cjs
+++ prisma/seed.cjs
@@ -11,7 +11,7 @@ async function main() {
       rewardRate: 0.00000000000000000001157407407407407407,
       contract: '0x74b988156925937bd4e082f0ed7429da8eaea8db',
       startingBlock: 13649693,
-      lastBlockInspected: 13942339,
+      lastBlockInspected: 13649692,
       positionSizeLimit: "100000000000000000000000"
     }
   });
@@ -25,7 +25,7 @@ async function main() {
       rewardRate: 0.00000000000000000001157407407407407407,
       contract: '0xd1790c5435b9fb7c9444c749cefe53d40d751eac',
       startingBlock: 22976955,
-      lastBlockInspected: 27015508,
+      lastBlockInspected: 22976954,
       positionSizeLimit: "100000000000000000000000"
     }
   });
@@ -39,7 +39,7 @@ async function main() {
       rewardRate: 0.0, // waiting for a response from the team
       contract: '0x230319cbd36fd911c5537254c99613b447b8a178',
       startingBlock: 25732987,
-      lastBlockInspected: 27015508,
+      lastBlockInspected: 25732986,
       positionSizeLimit: "100000000000000000000000"
     }
   });
