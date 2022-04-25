7636b45d804d959505ed11a8c1feb4ce7bf64e2c
Fornax <23104993+fornax2@users.noreply.github.com>
2022-04-25 14:40:32 -0300

    adding maxTicketsPerUser

---
+++ prisma/migrations/20220425173915_add_max_tickets_per_user/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Changed the type of `endTime` on the `Lottery` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `startTime` on the `Lottery` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Lottery" ADD COLUMN     "maxTicketsPerUser" INTEGER NOT NULL DEFAULT 0,
+DROP COLUMN "endTime",
+ADD COLUMN     "endTime" TIMESTAMP(3) NOT NULL,
+DROP COLUMN "startTime",
+ADD COLUMN     "startTime" TIMESTAMP(3) NOT NULL;
