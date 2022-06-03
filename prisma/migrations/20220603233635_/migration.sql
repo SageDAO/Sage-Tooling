e9999ba6056e7273eba32d8013f20aec67139e21
Fornax <23104993+fornax2@users.noreply.github.com>
2022-06-03 20:37:03 -0300

    remove erc20address

---
+++ prisma/migrations/20220603233635_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `erc20Address` on the `Auction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Auction" DROP COLUMN "erc20Address";
