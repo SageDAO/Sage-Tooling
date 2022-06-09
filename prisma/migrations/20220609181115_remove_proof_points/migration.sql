17e04be4f4a97a063f4a39056f4912c1a91d1170
Fornax <23104993+fornax2@users.noreply.github.com>
2022-06-09 16:38:02 -0300

    removed proofs from points earned

---
+++ prisma/migrations/20220609181115_remove_proof_points/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `proof` on the `EarnedPoints` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EarnedPoints" DROP COLUMN "proof";
